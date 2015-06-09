aac99b38dd86d29d8b79bec9bd654fe17351ece0
cdracars <cdracars@gmail.com>
2015-06-09 17:12:21 -0500

    updated to current code on site

--- js/app.js
+++ js/app.js
@@ -15,17 +15,30 @@ angular.module('gpaCalc').controller('CalculatorController', function(){
         {letterGrade:'WF/F', numberGrade:0}
     ];
 
-    self.gpas =[];
+    self.hours = [
+        {numberHours:1},
+        {numberHours:2},
+        {numberHours:3},
+        {numberHours:4},
+        {numberHours:5},
+        {numberHours:6},
+        {numberHours:7},
+        {numberHours:8}
+    ];
+
+    self.gpas = [{
+        studentGrades:[{letterGrade:'', numberGrade:''}]
+    }];
 
     self.total = function(){
         var total = 0;
         var creditTotal = 0;
         var gradeAdd = 0;
         for (var i = 0; i < self.gpas.length; i++){
-            creditTotal += self.gpas[i].studentCredits;
-            gradeMultiple = self.gpas[i].studentGrades.numberGrade * self.gpas[i].studentCredits;
+            creditTotal += self.gpas[i].studentCredits.numberHours;
+            gradeMultiple = self.gpas[i].studentGrades.numberGrade * self.gpas[i].studentCredits.numberHours;
             gradeAdd += gradeMultiple;
-            currentCredit = self.gpas[i].studentCredits;
+            currentCredit = self.gpas[i].studentCredits.numberHours;
             currentGrade = self.gpas[i].studentGrades.numberGrade;
             if(!currentGrade || !currentCredit) {
                 return total;
@@ -39,9 +52,11 @@ angular.module('gpaCalc').controller('CalculatorController', function(){
     self.add = function(){
         self.gpas.push({
             studentGrades: {
-                numberGrade: ""
+                numberGrade:""
             },
-            studentCredits: ""
+            studentCredits: {
+                numberHours:""
+            }
         });
     }
 });
--- theme/ng_gpa_calculator.tpl.php
+++ theme/ng_gpa_calculator.tpl.php
@@ -7,22 +7,31 @@
  */
 ?>
 
-<div ng-app="gpaCalc">
+<div class="l-box" ng-app="gpaCalc">
+  <h2>Welcome to USAO’s GPA Calculator!</h2>
+  <p>Please use the calculator for an <em>estimated</em> GPA using credit hours, per class, and the grade you received in that class. Please remember that classes worth 0 credit hours do not count toward your GPA, nor do classes with a pass/fail grading system.</p>
+  <p><strong>Note</strong>: This is not an official GPA. For an official USAO GPA and transcript, please see the Registrar’s office, 204 Troutt Hall, or visit <a href="/current-students/requesting-usao-transcripts">our transcript page</a>.</p>
   <div ng-controller="CalculatorController as calc">
-    <div ng-repeat="gpa in calc.gpas track by $index">
-      <label>Course</label>
-      <input type="text" ng-model="gpa.studentCourses"></input>
-      <label>Credits</label>
-      <input type="number" min="1" ng-model="gpa.studentCredits"></input>
-      <label>Grade (null not allowed):</label>
-      <select ng-model="gpa.studentGrades"
+    <div class="form-item calc-row" ng-repeat="gpa in calc.gpas track by $index">
+      <strong>Class {{ $index + 1 }}</strong>
+      <label class="calc-label" for="hours">Hours:</label>
+      <select class="calc-select" id="hours" ng-model="gpa.studentCredits"
+              ng-options="hour.numberHours for hour in calc.hours"></select><br>
+      <label class="calc-label" for="grade">Grade:</label>
+      <select class="calc-select" id="grade" ng-model="gpa.studentGrades"
               ng-options="grade.letterGrade for grade in calc.grades"></select><br>
     </div>
-    <hr/>
-    total: {{ calc.total(gpa.studentGrades) }}<br>
-
-    <div>
-      <button ng-click="calc.add()">add</button>
+    <div class="form-item">
+      <button ng-click="calc.add()">Add Another Class</button>
     </div>
-  </div>
+    <p>Calculated GPA: {{ calc.total(gpa.studentGrades) | number:2 }}</p>
+  </div> <!-- End Calculator -->
+  <p><strong>How to calculate your GPA:</strong></p>
+  <ul>
+    <li>Step 1 – List the number of hours for each course.</li>
+    <li>Step 2 – List the grades on a 4.0 scale. (A=4, B=3, C=2, D=1)</li>
+    <li>Step 3 – Multiply the number of hours by the grade.</li>
+    <li>Step 4 – Add the grade points.</li>
+    <li>Step 5 – Divide the total grade points by the total number of credit hours.</li>
+  </ul>
 </div>
